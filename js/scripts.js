4e02b590a163702640d94cb9f1170d85d551e1d0
laurantbb <44342128+laurantbb@users.noreply.github.com>
2023-01-15 22:51:33 +0100

    New Project

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,39 @@
 // Document Ready
-$(document).ready(function() {
+$(document).ready(function () {
+    var imageMain = document.getElementsByClassName('profile-image');
+    new simpleParallax(imageMain, {
+        orientation: 'right'
+    });
+    var image0 = document.getElementsByClassName('img-0');
+    new simpleParallax(image0, {
+        orientation: 'left'
+    });
 
+    var image1 = document.getElementsByClassName('img-1');
+    new simpleParallax(image1, {
+        overflow: true, delay: .3
+    });
+
+    var image2 = document.getElementsByClassName('img-2');
+    new simpleParallax(image2, {
+        overflow: true, delay: .3
+    });
+
+    var image3 = document.getElementsByClassName('img-3');
+    new simpleParallax(image3, {
+        overflow: true, delay: .3
+    });
+
+    var image4 = document.getElementsByClassName('img-4');
+    new simpleParallax(image4, {
+        orientation: 'right'
+    });
 });
+
+
+ScrollReveal().reveal(".skills-section", { delay: 200 });
+ScrollReveal().reveal(".sponsors-section", { delay: 200 });
+ScrollReveal().reveal(".image-section", { delay: 200 });
+ScrollReveal().reveal(".plan-section", { delay: 200 });
+ScrollReveal().reveal(".careers-section", { delay: 200 });
+ScrollReveal().reveal(".footer-section", { delay: 200 });
